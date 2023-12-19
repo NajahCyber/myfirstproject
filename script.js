c9f0089cfa062cdef4eeb3b6da110ade77bb1c63
Abeer <engineera209@gmail.com>
2023-12-19 21:06:09 +0300

    the full version of task managment

--- script.js
+++ script.js
@@ -12,17 +12,63 @@ function displayTasks() {
   tasks.forEach(task => {
     const li = document.createElement('li');
     li.classList.add('task');
-    li.textContent = task.description;
+    
+    const nameSpan = document.createElement('span');
+    nameSpan.textContent = task.name.slice(0, 20);
+    nameSpan.classList.add('task-name');
+    li.appendChild(nameSpan);
+    
+    const description = document.createElement('span');
+    description.textContent = task.description;
+    description.classList.add('task-description');
+    description.style.display = 'none';
+    li.appendChild(description);
 
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.onclick = () => deleteTask(task.id);
-    li.appendChild(deleteButton);
+    li.addEventListener('click', () => {
+      if (description.style.display === 'none') {
+        description.style.display = 'block';
+      } else {
+        description.style.display = 'none';
+      }
+    });
 
     const editButton = document.createElement('button');
     editButton.textContent = 'Edit';
     editButton.onclick = () => editTask(task.id);
-    li.appendChild(editButton);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.onclick = () => deleteTask(task.id);
+
+    const editDeleteContainer = document.createElement('div');
+    editDeleteContainer.classList.add('task-buttons');
+    editDeleteContainer.appendChild(editButton);
+    editDeleteContainer.appendChild(deleteButton);
+
+    const moveButtonsContainer = document.createElement('div');
+    moveButtonsContainer.classList.add('task-buttons');
+
+    switch (task.status) {
+      case 'todo':
+        const inProgressButton = createMoveButton('Move to In Progress', 'inProgress', task.id);
+        moveButtonsContainer.appendChild(inProgressButton);
+        break;
+      case 'inProgress':
+        const completeButton = createMoveButton('Move to Completed', 'completed', task.id);
+        moveButtonsContainer.appendChild(completeButton);
+        const backToTodoButton = createMoveButton('Move back to To Do', 'todo', task.id);
+        moveButtonsContainer.appendChild(backToTodoButton);
+        break;
+      case 'completed':
+        const backToInProgressButton = createMoveButton('Move back to In Progress', 'inProgress', task.id);
+        moveButtonsContainer.appendChild(backToInProgressButton);
+        break;
+      default:
+        break;
+    }
+
+    li.appendChild(editDeleteContainer);
+    li.appendChild(moveButtonsContainer);
 
     switch (task.status) {
       case 'todo':
@@ -38,17 +84,42 @@ function displayTasks() {
         break;
     }
   });
+
+  toggleNoTaskMessage(todoList);
+  toggleNoTaskMessage(inProgressList);
+  toggleNoTaskMessage(completedList);
+}
+
+function toggleNoTaskMessage(taskList) {
+  const noTaskMessage = taskList.parentElement.querySelector('.no-task-msg');
+  if (taskList.children.length === 0) {
+    noTaskMessage.style.display = 'block';
+  } else {
+    noTaskMessage.style.display = 'none';
+  }
+}
+
+function createMoveButton(text, targetStatus, taskId) {
+  const button = document.createElement('button');
+  button.textContent = text;
+  button.classList.add('task-button');
+  button.onclick = () => moveTask(taskId, targetStatus);
+  return button;
 }
 
 function addTask() {
-  const taskInput = document.getElementById('taskInput');
+  const taskNameInput = document.getElementById('taskNameInput');
+  const taskDescriptionInput = document.getElementById('taskDescriptionInput');
+
   const newTask = {
     id: Date.now(),
-    description: taskInput.value,
+    name: taskNameInput.value,
+    description: taskDescriptionInput.value,
     status: 'todo',
   };
   tasks.push(newTask);
-  taskInput.value = '';
+  taskNameInput.value = '';
+  taskDescriptionInput.value = '';
   displayTasks();
 }
 
@@ -58,23 +129,39 @@ function deleteTask(id) {
 }
 
 function editTask(id) {
-  const updatedDescription = prompt('Enter updated task description:');
-  if (updatedDescription !== null) {
+  const taskToEdit = tasks.find(task => task.id === id);
+
+  const updatedName = prompt('Enter updated task name:', taskToEdit.name);
+  if (updatedName !== null) {
+    const updatedDescription = prompt('Enter updated task description:', taskToEdit.description);
+
     tasks = tasks.map(task => {
       if (task.id === id) {
+        task.name = updatedName;
         task.description = updatedDescription;
       }
       return task;
     });
+
     displayTasks();
   }
 }
 
+function moveTask(taskId, newStatus) {
+  tasks = tasks.map(task => {
+    if (task.id === taskId) {
+      task.status = newStatus;
+    }
+    return task;
+  });
+  displayTasks();
+}
+
 // Sample tasks for demonstration purposes
 tasks = [
-  { id: 1, description: 'Task 1', status: 'todo' },
-  { id: 2, description: 'Task 2', status: 'inProgress' },
-  { id: 3, description: 'Task 3', status: 'completed' },
+  { id: 1, name: 'Task 1', description: 'Description for Task 1', status: 'todo' },
+  { id: 2, name: 'Task 2', description: 'Description for Task 2', status: 'inProgress' },
+  { id: 3, name: 'Task 3', description: 'Description for Task 3', status: 'completed' },
 ];
 
 displayTasks();
